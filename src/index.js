79277ae19ef011a080c613284ba1c01f7825319a
drew0007 <drew020777@gmail.com>
2024-08-04 23:26:07 +0900

    react context study

--- src/index.js
+++ src/index.js
@@ -10,10 +10,16 @@ import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <DarkOrLight />
   </React.StrictMode>,
   document.getElementById('root')
 );
